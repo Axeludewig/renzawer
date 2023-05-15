641542783e8f3698876921bd859e9e2ef66bd965
unknown <Sistemas@TICRAMIROPC.DataCenterLESP.local>
2023-05-15 14:29:50 -0600

    device

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -132,7 +132,7 @@ function Navbar() {
 								</a>
 							</li>
 							<li>
-								<div className="md:mr-0 mr-6">
+								<div className="md:mr-0 mr-4">
 									{!userId ? <SignInButton /> : ""}
 									{isLoaded ? (
 										<UserButton />
